ac95de5b09d817dfd8865b868f3975b05c460c29
Thy <thierry@ovh1>
2016-01-18 18:49:40 +0100

    some new entries

--- georeference.js
+++ georeference.js
@@ -8,5 +8,30 @@
 	    "longitude": "82°38′24″W",
 	    "latitude": "27°46′23″N"
 	}
+    },
+    "Southern Australia": {
+	"source": "https://en.wikipedia.org/wiki/Southern_Australia",
+	"info": {
+	    "countries": ["Australia"],
+	    "longitude": "142°23′37.73″E",
+	    "latitude": "31°24′23.43″S"
+	}
+    },
+    "London (Central), London (Greater) (GB)": {
+	"source": "https://en.wikipedia.org/wiki/London",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°7′39″W",
+	    "latitude": "51°30′26″N"
+	}
+    },
+    "Royston, England": {
+	"source": "https://en.wikipedia.org/wiki/Royston,_Hertfordshire",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°1′12.72″W",
+	    "latitude": "52°2′49.56″N"
+	}
     }
 }
+
