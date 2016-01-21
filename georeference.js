1cead448086ecb47a2b87014d6bb278ace822573
Thy <thierry@ovh1>
2016-01-21 10:20:44 +0100

    some new entries

--- georeference.js
+++ georeference.js
@@ -25,6 +25,14 @@
 	    "latitude": "51°30′26″N"
 	}
     },
+    "GBR": {
+	"source": "https://en.wikipedia.org/wiki/London",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°7′39″W",
+	    "latitude": "51°30′26″N"
+	}
+    },    
     "Royston, England": {
 	"source": "https://en.wikipedia.org/wiki/Royston,_Hertfordshire",
 	"info": {
@@ -32,6 +40,110 @@
 	    "longitude": "0°1′12.72″W",
 	    "latitude": "52°2′49.56″N"
 	}
-    }
+    },
+    "Cambridgeshire": {
+	"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°0′1″E",
+	    "latitude": "52°20′0″N"
+	}
+    },
+    "Cambridgeshire, England": {
+	"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°0′1″E",
+	    "latitude": "52°20′0″N"
+	}
+    },
+    "Cambridge, Cambridgeshire (GB)": {
+	"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°0′1″E",
+	    "latitude": "52°20′0″N"
+	}
+    },
+    "Cambridgeshire (GB)": {
+	"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+	"info": {
+	    "countries": ["England"],
+	    "longitude": "0°0′1″E",
+	    "latitude": "52°20′0″N"
+	}
+    },    
+    "USA - Ohio": {
+	"source": "https://en.wikipedia.org/wiki/Columbus,_Ohio",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "82°59′0″W",
+	    "latitude": "39°59′0″N"
+	}
+    },
+    "USA - Nevada": {
+	"source": "https://en.wikipedia.org/wiki/Carson_City,_Nevada",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "119°45′14″W",
+	    "latitude": "39°9′39″N"
+	}
+    },
+    "USA - Michigan": {
+	"source": "https://en.wikipedia.org/wiki/Lansing,_Michigan",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "84°32′48″W",
+	    "latitude": "42°44′1″N"
+	}
+    },
+    "USA - Virginia": {
+	"source": "https://en.wikipedia.org/wiki/Richmond,_Virginia",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "77°28′0″W",
+	    "latitude": "37°32′0″N"
+	}
+    },
+    "USA - Virginia": {
+	"source": "https://en.wikipedia.org/wiki/Richmond,_Virginia",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "77°28′0″W",
+	    "latitude": "37°32′0″N"
+	}
+    },
+    "USA - Louisiana": {
+	"source": "https://en.wikipedia.org/wiki/Baton_Rouge,_Louisiana",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "91°8′24″W",
+	    "latitude": "30°27′0″N"
+	}
+    },
+    "USA - Oklahoma": {
+	"source": "https://en.wikipedia.org/wiki/Oklahoma_City",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "97°32′6″W",
+	    "latitude": "35°28′56″N"
+	}
+    },
+    "TN": {
+	"source": "https://en.wikipedia.org/wiki/Memphis,_Tennessee",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "89°58′15.85″W",
+	    "latitude": "35°7′2.51″N"
+	}
+    },
+    "USA": {
+	"source": "https://en.wikipedia.org/wiki/United_States",
+	"info": {
+	    "countries": ["USA"],
+	    "longitude": "100°0′0″W",
+	    "latitude": "40°0′0″N"
+	}
+    }    
 }
 
