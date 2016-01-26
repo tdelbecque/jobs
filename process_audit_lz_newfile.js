afbac56cbd20c6f28f91c0eb108926da5c378ae9
Thy <thierry@ovh1>
2016-01-26 16:30:39 +0100

    lift diagnostic & parameters in the mail

--- process_audit_lz_newfile.js
+++ process_audit_lz_newfile.js
@@ -13,28 +13,60 @@ var fresherFile = u.getFresherFileSync ('/home/jobs/lz', filter);
 
 if (fresherFile !== undef) {
     var timeStamp = /M_campaignlist-(\d+)\.csv/.exec (fresherFile) [1];
-    var sectorsFilePath = '/home/jobs/lz/sectors-' + timeStamp + '.csv';
-    var jobsFreqsFilePath = '/home/jobs/lz/jobsfreqs-' + timeStamp + '.csv';
-    cp.exec ('wc -l /home/jobs/lz/' + fresherFile,
-	     function (err, stdout, strerr) {
-		 var body = '';
-		 var sectorTables = '';
-		 var jobsFreqs = '';
-		 var newFileSize = stdout;
+    var lzdir = '/home/jobs/lz/'
+    var sectorsFilePath = lzdir + 'sectors-' + timeStamp + '.csv';
+    var jobsFreqsFilePath = lzdir + 'jobsfreqs-' + timeStamp + '.csv';
+    var diagnosticFilePath = lzdir + 'Diagnostic_campaignlist-' + timeStamp + '.csv';
+    var parametersFilePath = lzdir + 'parameters-' + timeStamp + '.csv';
+    var nblines = '?'
+    var is_UTF8 = '?'
+    var body = '';
+    var sectorTables = '';
+    var jobsFreqs = '';
+    var parameters = ''
+    
+    try {
+	var drows = fs.readFileSync (diagnosticFilePath).toString().split ('\n')
+	nblines = /\d+/.exec (drows [0])[0]
+	is_UTF8 = (drows [1].indexOf ('UTF-8') >= 0)
+    }
+    catch (e) {
+    }
+
+    try {
+	parameters =
+	    '\n\n' +
+	    fs.readFileSync (parametersFilePath).
+	    toString ().
+	    replace (/\t/g, ': ') +
+	    '\n\n'
+    }
+    catch (e) {
+	parameters = ''
+    }
+    
+    if (fs.statSync (sectorsFilePath)) {
+	sectorsTable = fs.
+	    readFileSync (sectorsFilePath).
+	    toString ()}
+    
+    if (fs.statSync (jobsFreqsFilePath)) {
+	jobsFreqs = fs.
+	    readFileSync (jobsFreqsFilePath).
+	    toString ().
+	    split ('\n').
+	    slice (0, 10).
+	    join ('\n')}
+    
+    body = 'Timestamp: ' + timeStamp +
+	parameters +
+	'Nb lines: ' + nblines +
+	'\n\nIs UTF-8: ' + is_UTF8 +
+	'\n\nSECTORS:\n\n' + sectorsTable +
+	'\n\nTOP 10 JOBS FREQUENCIES:\n\n' + jobsFreqs +
+	'\n\n';
 		 
-		 if (fs.statSync (sectorsFilePath)) {
-		     sectorsTable = fs.readFileSync (sectorsFilePath).toString ();
-		 }
-		 if (fs.statSync (jobsFreqsFilePath)) {
-		     jobsFreqs = fs.readFileSync (jobsFreqsFilePath).toString ().split ('\n').slice (0, 10).join ('\n');
-		 }
-		     
-		 body = newFileSize +
-		     '\n\nSECTORS:\n\n' + sectorsTable +
-		     '\n\nTOP 10 JOBS FREQUENCIES:\n\n' + jobsFreqs +
-		     '\n\n';
-		 
-		 mail.sendMeMail ('New file on LZ', body)})
+    mail.sendMeMail ('New file on LZ', body)
 } else {
     mail.sendMeMail ('New file on LZ', 'Unable to get the fresher file')
 }
