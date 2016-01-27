1a8cf4f843aafcc74f67f2433ab16f84afc09751
Thy <thierry@ovh1>
2016-01-27 14:09:53 +0100

    produces stats on nb of jobs, expiry dates & sectors

--- digest-campfile.js
+++ digest-campfile.js
@@ -14,7 +14,6 @@ function digest (path, onFinish) {
     var self = this || {}
 
     self.path = path
-    self.arrayAsData = []
     self.emails = []
     self.job1_ids = []
     self.job2_ids = []
@@ -52,57 +51,87 @@ function digest (path, onFinish) {
 
 exports.digest = function (path) {return new digest (path)}
 
-if (!module.parent) {
-    if (process.argv.length !== 3) {
-	console.error (process.argv [1] + " (file to analyse)")
-	return -1;
-    }
-	
-    var filein = process.argv [2]
-
-    function onFinish (dg) {
+function analyse (filein, onFinish) {
+    function onDigestFinish (dg) {
 	var jobsdb = EXPORT.aggregateData ()
 	var extractfun = jobsdb.extractfun
 
 	var allDates = {}
-	function updateAllDates (dates, times) {
-	    dates.forEach (function (d, i) {allDates [d] = times [i]})
+	var allSectorsTable = {}
+	
+	function extractForRank (i) {
+	    function updateAllDates (dates, times) {
+		dates.forEach (function (d, i) {allDates [d] = times [i]})
+	    }
+	    function feat (what) {return 'job' + i + '_' + what}
+	    var ids = dg ['job' + i + '_ids']
+	    
+	    dg [feat ('expiryTimes')] = ids.map (extractfun ('expiryTime'))
+	    dg [feat ('expiryDates')] = ids.map (extractfun ('expiryDate'))
+	    updateAllDates (dg [feat ('expiryDates')], dg [feat ('expiryTimes')])
+	    dg [feat ('expiryDatesTable')] = UTILS.table (dg [feat ('expiryDates')])
+	    var flattenSectors = ids.map (extractfun ('flattenSectors'))
+	    var st = dg [feat ('sectorsTable')] = {}
+	    flattenSectors.forEach (function (x) {
+		x.split (';').forEach (function (x) {
+		    allSectorsTable [x] = (allSectorsTable [x] || 0) + 1
+		    st [x] = (st [x] || 0) + 1 })})
 	}
-	
-	dg.job1_expiryTimes = dg.job1_ids.map (extractfun ('expiryTime'))
-	dg.job1_expiryDates = dg.job1_ids.map (extractfun ('expiryDate'))
-	updateAllDates (dg.job1_expiryDates, dg.job1_expiryTimes)
-	dg.job1_expiryDatesTable = UTILS.table (dg.job1_expiryDates)
-	
-	dg.job2_expiryTimes = dg.job2_ids.map (extractfun ('expiryTime'))
-	dg.job2_expiryDates = dg.job2_ids.map (extractfun ('expiryDate'))
-	updateAllDates (dg.job2_expiryDates, dg.job2_expiryTimes)
-	dg.job2_expiryDatesTable = UTILS.table (dg.job2_expiryDates)
-	
-	dg.job3_expiryTimes = dg.job3_ids.map (extractfun ('expiryTime'))
-	dg.job3_expiryDates = dg.job3_ids.map (extractfun ('expiryDate'))
-	updateAllDates (dg.job3_expiryDates, dg.job3_expiryTimes)
-	dg.job3_expiryDatesTable = UTILS.table (dg.job3_expiryDates)
 
-	dg.expiryDatesReport = Object.keys (allDates).
+	[1,2,3].forEach (extractForRank)
+
+	function tableRowBuilder (xs) {
+	    return function (rowname) {
+		return [rowname].concat (xs.map (function (x) {return x [rowname] || 0}))}}
+
+	function feat (what) {
+	    return function (i) {return dg ['job' + i + '_' + what]}}
+	
+	dg.expiryDatesSpreadSheet = Object.keys (allDates).
 	    sort (function (a, b) {return allDates [a] - allDates [b]}).
-	    map (function (d) {
-		return d + '\t' +
-		    (dg.job1_expiryDatesTable [d] || 0) + '\t' +
-		    (dg.job2_expiryDatesTable [d] || 0) + '\t' +
-		    (dg.job3_expiryDatesTable [d] || 0)}).
-	    join ('\n')
+	    map (tableRowBuilder ([1,2,3].map (feat ('expiryDatesTable'))))
 
-	console.log (dg.expiryDatesReport)
+	dg.sectorsSpreadSheet = Object.keys (allSectorsTable).sort ().
+	    map (tableRowBuilder ([1,2,3].map (feat ('sectorsTable')).concat ([allSectorsTable])))
 
+	dg.jobIdTable = UTILS.table (dg.job1_ids.concat (dg.job2_ids.concat (dg.job3_ids)))
+				     
+	if (onFinish) onFinish (dg)
     }
     
     try {
-	new digest (filein, onFinish)
+	new digest (filein, onDigestFinish)
     }
     catch (e) {
 	console.error (e)
 	return -1
     }
 
+}
+
+if (!module.parent) {
+    if (process.argv.length !== 3) {
+	console.error (process.argv [1] + " (file to analyse)")
+	return -1;
+    }
+
+    function onFinish (dg) {
+ 	function toString (x) {
+	    return x.map (function (x) {return x.join ('\t')}).join ('\n')}
+
+	console.log ('' + Object.keys (dg.jobIdTable).length + ' unique jobs')
+	Object.keys (dg.jobIdTable).
+	    sort (function (a, b) {return dg.jobIdTable [b] - dg.jobIdTable [a]}).
+	    slice (0, 10).
+	    forEach (function (id) {console.log (id + '\t' + dg.jobIdTable [id])})
+	
+	console.log ('\nExpiry dates')
+	console.log (toString (dg.expiryDatesSpreadSheet))
+	console.log ('\nSectors')
+	console.log (toString (dg.sectorsSpreadSheet))
+   }
+    
+    var filein = process.argv [2]
+
+    analyse (filein, onFinish)
 }
