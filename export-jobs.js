42df537656f8235e4ce5ff521d5dca0656a536bc
Thy <thierry@ovh1>
2016-02-25 12:15:09 +0100

    compute country from latitude longitude

--- export-jobs.js
+++ export-jobs.js
@@ -1,5 +1,6 @@
 var fs = require ('fs')
 var p = require ('./process')
+var http = require ('http')
 
 var undef
 var savedir = require ('path').dirname (process.argv [1]) + '/save/';
@@ -90,10 +91,79 @@ function writeJobs (jobs, file) {
 exports.writeJobs = writeJobs;
 
 function doExports () {
-    var x = m.jobsByIdsFile ('../campaigns/current/jobids-C3')
-    m.writeJobs (x, 'jobs-C3.tsv')
-    x = m.jobsByIdsFile ('../campaigns/20160201/jobids-C2')
-    m.writeJobs (x, 'jobs-C2.tsv')
-    x = m.jobsByIdsFile ('../campaigns/20160126/jobids-C1')
-    m.writeJobs (x, 'jobs-C1.tsv')
+    var countryCodes2to3 = {}
+    var countryCodes = fs.readFileSync ('countryCodes.csv').
+	toString ().
+	split ('\r\n').
+	filter (function (x) {return x !== ''}).
+	forEach (function (x) {var y = x.split (';'); countryCodes2to3 [y[1]] = y [2]})
+    
+    var dic = {}
+    var x3 = m.jobsByIdsFile ('../campaigns/current/jobids-C3')
+    x3.forEach (function (y) {dic ['' + y.latitude + ',' + y.longitude] = 1})
+    writeJobs (x3, 'jobs-C3.tsv')
+
+    var x2 = m.jobsByIdsFile ('../campaigns/20160201/jobids-C2')
+    x2.forEach (function (y) {dic ['' + y.latitude + ',' + y.longitude] = 1})
+    writeJobs (x2, 'jobs-C2.tsv')
+
+    var x1 = m.jobsByIdsFile ('../campaigns/20160126/jobids-C1')
+    x1.forEach (function (y) {dic ['' + y.latitude + ',' + y.longitude] = 1})
+    m.writeJobs (x1, 'jobs-C1.tsv')
+
+    xs = Object.keys (dic).map (function (x) {return {strLatitudeLongitude: x}})
+    googleCountryFromGeoIth (xs, 0)
+
+    var ctr = {}
+    xs.forEach (function (x) {ctr [x.strLatitudeLongitude] = x.country})
+    x1.forEach (function (x) {x.country = ctr ['' + x.latitude + ',' + x.longitude ]})
+    x2.forEach (function (x) {x.country = ctr ['' + x.latitude + ',' + x.longitude ]})
+    x3.forEach (function (x) {x.country = ctr ['' + x.latitude + ',' + x.longitude ]})
+    fs.writeFileSync ('jobscountries-C1', x1.map (function (x) {return [x.id, x.location, x.country].join ('\t')}).join ('\n'))
+    fs.writeFileSync ('jobscountries-C2', x2.map (function (x) {return [x.id, x.location, x.country].join ('\t')}).join ('\n'))
+    fs.writeFileSync ('jobscountries-C3', x3.map (function (x) {return [x.id, x.location, x.country].join ('\t')}).join ('\n'))
 }
+
+function googleCountryFromGeo (strLatitudeLongitude, state) {
+    onResponse = function (response) {
+	var body = '';
+	response.on ('data', function (d) {body += d})
+	response.on ('end', function () {
+	    var data = JSON.parse (body.toString ())
+	    state.country = data.results.map (function (x) {return x.address_components}).
+		filter (function (x) {return x}).
+		map (function (x) {return x.filter (function (x) {return x.types [0] === 'country'})})[0][0].short_name
+	})
+    }
+    var query = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' +
+	strLatitudeLongitude;
+    
+    http.get (query, onResponse).on ('error', function (err) {console.log (err)})
+}
+
+exports.googleCountryFromGeo = googleCountryFromGeo;
+
+function googleCountryFromGeoIth (xs, i) {
+    if (i === xs.length) {
+	console.log ('Fini')
+	return
+    }
+    onResponse = function (response) {
+	var body = '';
+	response.on ('data', function (d) {body += d})
+	response.on ('end', function () {
+	    var data = JSON.parse (body.toString ())
+	    xs [i].country = data.results.map (function (x) {return x.address_components}).
+		filter (function (x) {return x}).
+		map (function (x) {return x.filter (function (x) {return x.types [0] === 'country'})})[0][0].short_name
+	    console.log (xs [i].strLatitudeLongitude + ' ==> ' + xs [i].country);
+	    setTimeout (function () {googleCountryFromGeoIth (xs, i + 1)}, 200)
+	})
+    }
+    var query = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' +
+	xs [i].strLatitudeLongitude;
+    
+    http.get (query, onResponse).on ('error', function (err) {console.log (err)})
+}
+
+exports.googleCountryFromGeoIth = googleCountryFromGeoIth
