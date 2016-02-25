d20ac6754b8e0b32889bf3b3ff2f5c6186026ee4
Thy <thierry@ovh1>
2016-02-25 14:52:32 +0100

    first commit

---
+++ ctrreference.js
@@ -0,0 +1,574 @@
+{
+	"Saint Petersburg, Florida": {
+		"source": "https://en.wikipedia.org/wiki/St._Petersburg,_Florida",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "82°38′24″W",
+			"latitude": "27°46′23″N"
+		},
+		"countryCode": "USA"
+	},
+	"Southern Australia": {
+		"source": "https://en.wikipedia.org/wiki/Southern_Australia",
+		"info": {
+			"countries": [
+				"Australia"
+			],
+			"longitude": "142°23′37.73″E",
+			"latitude": "31°24′23.43″S"
+		},
+		"countryCode": "AUS"
+	},
+	"London (Central), London (Greater) (GB)": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"South West London": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"London, SW": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"City of Westminster, London, SW1W 9SZ": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Various locations, UK": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"GBR": {
+		"source": "https://en.wikipedia.org/wiki/London",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Royston, England": {
+		"source": "https://en.wikipedia.org/wiki/Royston,_Hertfordshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°1′12.72″W",
+			"latitude": "52°2′49.56″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Cambridgeshire": {
+		"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "52°20′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Cambridgeshire, England": {
+		"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "52°20′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Cambridge, Cambridgeshire (GB)": {
+		"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "52°20′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Cambridgeshire (GB)": {
+		"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "52°20′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Hinxton, nr Cambridge, UK": {
+		"source": "https://en.wikipedia.org/wiki/Cambridgeshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "52°20′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Birmingham, West Midlands": {
+		"source": "https://fr.wikipedia.org/wiki/Birmingham",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "1°53′37″W",
+			"latitude": "52°28′59″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Midlands, UK": {
+		"source": "https://fr.wikipedia.org/wiki/Birmingham",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "1°53′37″W",
+			"latitude": "52°28′59″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Midlands": {
+		"source": "https://fr.wikipedia.org/wiki/Birmingham",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "1°53′37″W",
+			"latitude": "52°28′59″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Midlands - England": {
+		"source": "https://fr.wikipedia.org/wiki/Birmingham",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "1°53′37″W",
+			"latitude": "52°28′59″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Sussex, UK": {
+		"source": "https://en.wikipedia.org/wiki/Sussex",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°0′1″E",
+			"latitude": "51°0′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Yorkshire": {
+		"source": "https://en.wikipedia.org/wiki/Yorkshire",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°0′0″W",
+			"latitude": "54°0′0″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Vienna (Statutarstadt) (AT)": {
+		"source": "https://en.wikipedia.org/wiki/Vienna",
+		"info": {
+			"countries": [
+				"Austria"
+			],
+			"longitude": "16°22′0″E",
+			"latitude": "48°12′0″N"
+		},
+		"countryCode": "AUT"
+	},
+	"USA - Ohio": {
+		"source": "https://en.wikipedia.org/wiki/Columbus,_Ohio",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "82°59′0″W",
+			"latitude": "39°59′0″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA - Nevada": {
+		"source": "https://en.wikipedia.org/wiki/Carson_City,_Nevada",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "119°45′14″W",
+			"latitude": "39°9′39″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA - Michigan": {
+		"source": "https://en.wikipedia.org/wiki/Lansing,_Michigan",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "84°32′48″W",
+			"latitude": "42°44′1″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA - Virginia": {
+		"source": "https://en.wikipedia.org/wiki/Richmond,_Virginia",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "77°28′0″W",
+			"latitude": "37°32′0″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA - Louisiana": {
+		"source": "https://en.wikipedia.org/wiki/Baton_Rouge,_Louisiana",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "91°8′24″W",
+			"latitude": "30°27′0″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA - Oklahoma": {
+		"source": "https://en.wikipedia.org/wiki/Oklahoma_City",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "97°32′6″W",
+			"latitude": "35°28′56″N"
+		},
+		"countryCode": "USA"
+	},
+	"TN": {
+		"source": "https://en.wikipedia.org/wiki/Memphis,_Tennessee",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "89°58′15.85″W",
+			"latitude": "35°7′2.51″N"
+		},
+		"countryCode": "USA"
+	},
+	"Metro - NY City": {
+		"source": "https://en.wikipedia.org/wiki/New_York_City",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "74°0′21.24″W",
+			"latitude": "40°42′45.72″N"
+		},
+		"countryCode": "USA"
+	},
+	"USA": {
+		"source": "https://en.wikipedia.org/wiki/United_States",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "100°0′0″W",
+			"latitude": "40°0′0″N"
+		},
+		"countryCode": "USA"
+	},
+	"Washington": {
+		"source": "https://fr.wikipedia.org/wiki/Washington_%28district_de_Columbia%29",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "77°2′11.64″W",
+			"latitude": "38°53′42.37″N"
+		},
+		"countryCode": "USA"
+	},
+	"District of Columbia": {
+		"source": "https://fr.wikipedia.org/wiki/Washington_%28district_de_Columbia%29",
+		"info": {
+			"countries": [
+				"USA"
+			],
+			"longitude": "77°2′11.64″W",
+			"latitude": "38°53′42.37″N"
+		},
+		"countryCode": "USA"
+	},
+	"SWE": {
+		"source": "https://en.wikipedia.org/wiki/Stockholm",
+		"info": {
+			"countries": [
+				"Sweden"
+			],
+			"longitude": "18°4′7″E",
+			"latitude": "59°19′46″N"
+		},
+		"countryCode": "SWE"
+	},
+	"SGP": {
+		"source": "https://en.wikipedia.org/wiki/Singapore",
+		"info": {
+			"countries": [
+				"Singapore"
+			],
+			"longitude": "103°48′0″E",
+			"latitude": "1°18′0″N"
+		},
+		"countryCode": "SGP"
+	},
+	"Portishead": {
+		"source": "https://en.wikipedia.org/wiki/Portishead,_Somerset",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°45′45.36″W",
+			"latitude": "51°29′2.4″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Danvers, Massachusetts": {
+		"source": "https://en.wikipedia.org/wiki/Danvers,_Massachusetts",
+		"info": {
+			"countries": [
+				"United States"
+			],
+			"longitude": "70°55′50″W",
+			"latitude": "42°34′30″N"
+		},
+		"countryCode": "USA"
+	},
+	"Ann Arbor, US": {
+		"source": "https://en.wikipedia.org/wiki/Ann_Arbor,_Michigan",
+		"info": {
+			"countries": [
+				"United States"
+			],
+			"longitude": "83°44′54″W",
+			"latitude": "42°16′53″N"
+		},
+		"countryCode": "USA"
+	},
+	"Ann Arbor Michigan": {
+		"source": "https://en.wikipedia.org/wiki/Ann_Arbor,_Michigan",
+		"info": {
+			"countries": [
+				"United States"
+			],
+			"longitude": "83°44′54″W",
+			"latitude": "42°16′53″N"
+		},
+		"countryCode": "USA"
+	},
+	"Ann Arbor, Michigan": {
+		"source": "https://en.wikipedia.org/wiki/Ann_Arbor,_Michigan",
+		"info": {
+			"countries": [
+				"United States"
+			],
+			"longitude": "83°44′54″W",
+			"latitude": "42°16′53″N"
+		},
+		"countryCode": "USA"
+	},
+	"North West England, England": {
+		"source": "https://en.wikipedia.org/wiki/North_West_England",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°38′00″W",
+			"latitude": "54°01′00″N"
+		},
+		"countryCode": "GBR"
+	},
+	"North West England": {
+		"source": "https://en.wikipedia.org/wiki/North_West_England",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°38′00″W",
+			"latitude": "54°01′00″N"
+		},
+		"countryCode": "GBR"
+	},
+	"North West England (GB)": {
+		"source": "https://en.wikipedia.org/wiki/North_West_England",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°38′00″W",
+			"latitude": "54°01′00″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Kingston Upon Hull, England": {
+		"source": "https://en.wikipedia.org/wiki/Kingston_upon_Hull",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°19′57″W",
+			"latitude": "53°44′39.6″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Kingston upon Hull, East Riding of Yorkshire (GB)": {
+		"source": "https://en.wikipedia.org/wiki/Kingston_upon_Hull",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°19′57″W",
+			"latitude": "53°44′39.6″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Leek": {
+		"source": "https://en.wikipedia.org/wiki/Laken",
+		"info": {
+			"countries": [
+				"Belgium"
+			],
+			"longitude": "4°21′0″E",
+			"latitude": "50°52′0″N"
+		},
+		"countryCode": "BEL"
+	},
+	"China": {
+		"source": "https://en.wikipedia.org/wiki/China",
+		"info": {
+			"countries": [
+				"\n\n\nBrazil\nCanada\nEstonia\nFrance\nIndia\nNetherlands\nNew Zealand\nPakistan\nUnited Kingdom\nUnited States\n\n\n"
+			],
+			"longitude": "103°E",
+			"latitude": "35°N"
+		},
+		"countryCode": "CHN"
+	},
+	"Neuchâtel": {
+		"source": "https://en.wikipedia.org/wiki/Neuchatel_NE",
+		"info": {
+			"countries": [
+				"Switzerland"
+			],
+			"longitude": "6°56′E",
+			"latitude": "47°0′N"
+		},
+		"countryCode": "CHE"
+	},
+	"Dusseldorf": {
+		"source": "https://en.wikipedia.org/wiki/D%C3%BCsseldorf",
+		"info": {
+			"countries": [
+				"Germany"
+			],
+			"longitude": "6°47′0″E",
+			"latitude": "51°14′0″N"
+		},
+		"countryCode": "DEU"
+	},
+	"Europe": {
+		"source": "",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "0°7′39″W",
+			"latitude": "51°30′26″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Northern England": {
+		"source": "https://en.wikipedia.org/wiki/North_West_England",
+		"info": {
+			"countries": [
+				"England"
+			],
+			"longitude": "2°38′00″W",
+			"latitude": "54°01′00″N"
+		},
+		"countryCode": "GBR"
+	},
+	"Munich (Landkreis), Bayern (DE)": {
+		"source": "https://en.wikipedia.org/wiki/Munich",
+		"info": {
+			"countries": [
+				"Germany"
+			],
+			"longitude": "11°34′0″E",
+			"latitude": "48°8′0″N"
+		},
+		"countryCode": "DEU"
+	},
+	"Belfast (City/Town) (GB)": {
+		"source": "https://en.wikipedia.org/wiki/Belfast",
+		"info": {
+			"countries": [
+				"Irland"
+			],
+			"longitude": "5°55′48″W",
+			"latitude": "54°35′49.2″N"
+		},
+		"countryCode": "GBR"
+	}
+}
