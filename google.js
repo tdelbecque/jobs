b33a024ae7b6cef12cc675e8ae6d340eaff0d160
Thy <thierry@ovh1>
2016-02-25 14:39:32 +0100

    ask google for ctrcode; keep ctr db in sync with the last geo db

--- google.js
+++ google.js
@@ -1,5 +1,7 @@
+var fs = require ('fs')
 var https = require ('https')
 var p = require ('./process')
+var utils = require ('./utils')
 
 process.on('uncaughtException', function (err) {
     var message = 'uncaughtException in jobs download process: ' +
@@ -8,6 +10,8 @@ process.on('uncaughtException', function (err) {
     console.error(message);
 });
 
+var rootdir = require ('path').dirname (process.argv [1]);
+var savedir = rootdir + '/save';
 var undef;
 
 function GoogleIth (xs, i, onFinish) {
@@ -66,3 +70,77 @@ function foo (data) {
 }
 
 exports.foo = foo;
+
+function syncCountries () {
+    var lastGeoDB = utils.getFresherFileSync (
+	savedir,
+	function (f) {return /geodb-\d+\.json/.exec (f)})
+    
+    var lastCtrDB = utils.getFresherFileSync (
+	savedir,
+	function (f) {return /ctrdb-\d+\.json/.exec (f)})
+
+    var stamp = /\d+/.exec (lastGeoDB) [0]
+    var currentCtrDic = {}
+    
+    if (lastCtrDB !== undef) 
+	currentCtrDic = JSON.parse (fs.readFileSync (savedir + '/' + lastCtrDB).
+				    toString ())
+    
+    var currentGeoDic = {}
+    if (lastGeoDB !== undef)
+	currentGeoDic = JSON.parse (fs.readFileSync (savedir + '/' + lastGeoDB).
+				    toString ())
+
+    var toUpdateDic = {}
+
+    Object.keys (currentGeoDic).
+	filter (function (x) {return currentCtrDic [x] === undef}).
+	filter (function (x) {
+	    var y = currentGeoDic [x]
+	    return ! (isNaN (p.sexagesimalToDecimal (y.info.latitude)) || isNaN (p.sexagesimalToDecimal (y.info.longitude)))
+	}).
+	forEach (function (x) {toUpdateDic [x] = currentGeoDic [x]})
+
+    var countryCodes2to3 = {}
+    fs.readFileSync ('countryCodes.csv').
+	toString ().
+	split ('\r\n').
+	filter (function (x) {return x !== ''}).
+	forEach (function (x) {var y = x.split (';'); countryCodes2to3 [y[1]] = y [2]})
+
+    whenFinish = function (db) {
+	Object.keys (db).forEach (function (x) {currentCtrDic [x] = db [x]})
+	fs.writeFileSync (savedir + '/' + 'ctrdb-' + stamp + '.json', JSON.stringify (currentCtrDic))
+	console.log ('finish')
+    }
+    
+    googleCountryFromGeoIth (Object.keys(toUpdateDic), toUpdateDic, countryCodes2to3, 0, whenFinish)
+}
+
+exports.syncCountries = syncCountries
+
+function googleCountryFromGeoIth (xs, db, codemap, i, whenFinish) {
+    if (i === xs.length) {
+	whenFinish (db)
+	return
+    }
+    onResponse = function (response) {
+	var body = '';
+	response.on ('data', function (d) {body += d})
+	response.on ('end', function () {
+	    var data = JSON.parse (body.toString ())
+	    db [xs [i]].countryCode =
+		codemap [data.results.map (function (x) {return x.address_components}).
+			 filter (function (x) {return x}).
+			 map (function (x) {return x.filter (function (x) {return x.types [0] === 'country'})})[0][0].short_name]
+	    console.log (db [xs [i]].countryCode)
+	    setTimeout (function () {googleCountryFromGeoIth (xs, db, codemap, i + 1, whenFinish)}, 200)
+	})
+    }
+    var query = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' +
+	p.sexagesimalToDecimal (db [xs [i]].info.latitude) + ',' +
+	p.sexagesimalToDecimal (db [xs [i]].info.longitude);
+    console.log (query)
+    http.get (query, onResponse).on ('error', function (err) {console.log (err)})
+}
